d594a8626b931122862992e8e4f1a3fd09e7f34f
Mustafa_Farrag <76589515+Mustafa-Farrag@users.noreply.github.com>
2022-05-27 00:58:33 +0200

    delete added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,7 @@ import com.company.BTree.BTree;
 
 public class Main {
     public static void main(String[] args) {
-        BTree<Integer, String> t = new BTree<>(3);
-//        t.insert(10, "Mo");
-//        t.insert(20, "Fawzy");
-//        t.insert(5, "Abbas");
-//        t.insert(6, "Nour");
-//        t.insert(12, "Farrag");
-//        t.insert(30, "Gamal");
-//        t.insert(7, "FAANG");
-//        t.insert(15, "Mo");
-//        t.insert(29, "Fawzy");
-//        t.insert(1, "Abbas");
-//        t.insert(666, "Nour");
-//        t.insert(120, "Farrag");
-//        t.insert(300, "Gamal");
-//        t.insert(77, "FAANG");
+        BTree<Integer, String> t = new BTree<>(2);
         t.insert(1, "a");
         t.insert(11, "k");
         t.insert(8, "h");
@@ -27,32 +13,46 @@ public class Main {
 
         t.insert(6, "f");
         t.insert(26, "z");
-        t.insert(7, "g");
-        t.insert(3, "c");
-        t.insert(2, "b");
-        t.insert(25, "y");
-        t.insert(9, "i");
-        t.insert(5, "e");
-
-
-        t.insert(21, "u");
-        t.insert(12, "l");
-        t.insert(18, "r");
-        t.insert(24, "x");
-        t.insert(10, "j");
-
-
-        t.insert(15, "o");
-        t.insert(16, "p");
-        t.insert(17, "q");
-        t.insert(23, "w");
-
-        t.insert(19, "s");
-        t.insert(13, "m");
-        t.insert(20, "t");
-        t.insert(22, "v");
-
         System.out.println("Traversal of the constructed tree is ");
         t.traverse();
+        t.delete(4);
+        System.out.println("\n\n\nTraversal of the constructed tree is ");
+        t.traverse();
+//        t.insert(7, "g");
+//        t.insert(3, "c");
+//        t.insert(2, "b");
+//        t.insert(25, "y");
+//        t.insert(9, "i");
+//        t.insert(5, "e");
+//
+//        t.delete(14);
+
+//        t.insert(6, "f");
+//        t.insert(26, "z");
+//        t.insert(7, "g");
+//        t.insert(3, "c");
+//        t.insert(2, "b");
+//        t.insert(25, "y");
+//        t.insert(9, "i");
+//        t.insert(5, "e");
+//
+//
+//        t.insert(21, "u");
+//        t.insert(12, "l");
+//        t.insert(18, "r");
+//        t.insert(24, "x");
+//        t.insert(10, "j");
+//
+//
+//        t.insert(15, "o");
+//        t.insert(16, "p");
+//        t.insert(17, "q");
+//        t.insert(23, "w");
+//
+//        t.insert(19, "s");
+//        t.insert(13, "m");
+//        t.insert(20, "t");
+//        t.insert(22, "v");
+
     }
 }
